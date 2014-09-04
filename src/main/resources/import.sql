74cad1e23c7f4ee0d69ba981ea8105c096bf1b18
Richard Midwinter <rich@wansdyke-house.co.uk>
2014-09-04 13:13:18 +0100

    SQL import comment.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
+-- Initial data. Sets a username of smaash and password of password. Change it!
+
 insert into users (username, password, isadmin) values ('smaash', '$2a$12$UQ3PKhfRTtrbIk4yGLvP3.ZGmWMPkc66fyJ2t2guD61j.Ontp7c7a', true);
